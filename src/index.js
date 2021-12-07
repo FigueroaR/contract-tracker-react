e42ce535c90834b7a1961126f68c1f979a2e988a
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-12-06 21:02:59 -0500

    redux / dev tool / reducers set up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <React.StrictMode>
+<Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+</Provider>
+, document.getElementById('root'));
