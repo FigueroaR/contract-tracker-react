2ee717f0451c6b8987730c6905be58032480a96a
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-12-06 21:25:09 -0500

    actions set up

---
+++ src/actions/contract.js
@@ -0,0 +1,29 @@
+export const getContracts = () => {
+    return (dispatch) => {
+
+    }
+}
+
+export const getContract = () => {
+    return (dispatch) => {
+        
+    }
+}
+
+export const postContract = () => {
+    return (dispatch) => {
+        
+    }
+}
+
+export const editContract = () => {
+    return (dispatch) => {
+        
+    }
+}
+
+export const deleteContract = () => {
+    return (dispatch) => {
+        
+    }
+}
